cfc89a10a5a1aa35d8bf163e62c0af0b584366a9
zoecq001 <zoecq001@gmail.com>
2020-03-21 18:43:56 -0400

    set up

---
+++ src/rpc/SearchItem.java
@@ -0,0 +1,82 @@
+package rpc;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import database.MySQLConnection;
+import entity.Item;
+
+/**
+ * Servlet implementation class SearchItem
+ */
+@WebServlet("/search")
+public class SearchItem extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SearchItem() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		double lat = Double.parseDouble(request.getParameter("lat"));
+		double lon = Double.parseDouble(request.getParameter("lon"));
+		String userId = request.getParameter("user_id");
+		
+		// Term can be empty or null.
+		String term = request.getParameter("term");
+		
+		//database operation
+		MySQLConnection connection = new MySQLConnection();
+		try {
+			//put the search results into the items table
+			List<Item> items = connection.searchItems(lat, lon, term);
+			//load user's fav items
+			Set<String> favoriteItemIds = connection.getFavoriteItemIds(userId);
+
+			JSONArray array = new JSONArray();
+			for (Item item : items) {
+				JSONObject obj = item.toJSONObject();
+				obj.put("favorite", favoriteItemIds.contains(item.getItemId()));
+				array.put(obj);
+			}
+			RpcHelper.writeJsonArray(response, array);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			connection.close();
+		}
+
+	}
+
+
+
+
+
+
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+}
